22c57e4d5c871c57652ce3c10db09aff49713110
Iván López <lopezi@objectcomputing.com>
2021-07-19 11:58:00 +0200

    Switch to Reactor

--- src/main/java/example/micronaut/GreetingService.java
+++ src/main/java/example/micronaut/GreetingService.java
@@ -1,12 +1,12 @@
 package example.micronaut;
 
-import io.reactivex.Single;
 import jakarta.inject.Singleton;
+import reactor.core.publisher.Mono;
 
 @Singleton
 public class GreetingService {
 
-    public Single<Greeting> sayHi(String name) {
-        return Single.just(new Greeting("Hello " + name));
+    public Mono<Greeting> sayHi(String name) {
+        return Mono.just(new Greeting("Hello " + name));
     }
 }
